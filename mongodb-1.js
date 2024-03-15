6c2e3a1fb9f228c96bc562ebed0361afd16e9e18
DanielT08 <150853060+DanielT08@users.noreply.github.com>
2024-03-15 16:32:18 -0500

    Agregando script .js

---
+++ mongodb-1.js
@@ -0,0 +1,366 @@
+db.createCollection("user");
+
+// Insertar usuarios a la coleccion de uno en uno
+db.users.insertOne({
+  name: "Daniel",
+  Lastname: "Toro",
+  email: "daniel123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 1000,
+  age: 30,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "Samuel",
+  Lastname: "Mendoza",
+  email: "samuel123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 2000,
+  age: 18,
+  height: 160,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Luisa",
+  Lastname: "Marquez",
+  email: "luisa123@gmail.com",
+  city: "paris",
+  country: "francia",
+  salary: 3000,
+  age: 25,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Fernando",
+  Lastname: "Londoño",
+  email: "fernando123@gmail.com",
+  city: "londres",
+  country: "inglaterra",
+  salary: 4000,
+  age: 30,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "luz",
+  Lastname: "Londoño",
+  email: "luz123@gmail.com",
+  city: "madrid",
+  country: "españa",
+  salary: 3500,
+  age: 55,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Dania",
+  Lastname: "Toro",
+  email: "dania123@gmail.com",
+  city: "armenia",
+  country: "colombia",
+  salary: 3000,
+  age: 26,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "David",
+  Lastname: "Toro",
+  email: "david123@gmail.com",
+  city: "new york",
+  country: "estados unidos",
+  salary: 3000,
+  age: 27,
+  height: 180,
+  weight: 130,
+});
+
+db.users.insertOne({
+  name: "Dario",
+  Lastname: "Gomez",
+  email: "dario123@gmail.com",
+  city: "londres",
+  country: "inglaterra",
+  salary: 3000,
+  age: 31,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "Diomedes",
+  Lastname: "Diaz",
+  email: "diomedes123@gmail.com",
+  city: "sidney",
+  country: "australia",
+  salary: 3000,
+  age: 29,
+  height: 150,
+  weight: 150,
+});
+
+db.users.insertOne({
+  name: "Raul",
+  Lastname: "Reyes",
+  email: "raul123@gmail.com",
+  city: "ontario",
+  country: "canada",
+  salary: 1500,
+  age: 41,
+  height: 140,
+  weight: 90,
+});
+
+db.users.insertOne({
+  name: "Carlos",
+  Lastname: "Lether",
+  email: "carlos123@gmail.com",
+  city: "quebed",
+  country: "canada",
+  salary: 5000,
+  age: 37,
+  height: 190,
+  weight: 190,
+});
+
+db.users.insertOne({
+  name: "Santiago",
+  Lastname: "Giraldo",
+  email: "santiago123@gmail.com",
+  city: "roma",
+  country: "italia",
+  salary: 3000,
+  age: 28,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Felipe",
+  Lastname: "Gridales",
+  city: "lima",
+  country: "peru",
+  salary: 2000,
+  age: 20,
+  height: 160,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Daniel",
+  Lastname: "Montiel",
+  email: "danie123@gmail.com",
+  city: "paris",
+  country: "francia",
+  salary: 4000,
+  age: 28,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "alberto",
+  Lastname: "Montiel",
+  email: "alberto123@gmail.com",
+  city: "rio",
+  country: "brasil",
+  salary: 3000,
+  age: 30,
+  height: 180,
+  weight: 150,
+});
+
+db.users.insertOne({
+  name: "Jose",
+  Lastname: "Acevedo",
+  email: "jose123@gmail.com",
+  city: "santiago",
+  country: "chile",
+  salary: 2000,
+  age: 21,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Pedro",
+  Lastname: "Restrepo",
+  email: "pedro123@gmail.com",
+  city: "buenos aires",
+  country: "argentina",
+  salary: 3000,
+  age: 20,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "carlos",
+  Lastname: "Restrepo",
+  email: "carlosr123@gmail.com",
+  city: "barselona",
+  country: "españa",
+  salary: 2000,
+  age: 25,
+  height: 170,
+  weight: 130,
+});
+
+db.users.insertOne({
+  name: "Pipe",
+  Lastname: "Rojas",
+  email: "pipe123@gmail.com",
+  city: "ciudad de mexico",
+  country: "mexico",
+  salary: 2500,
+  age: 30,
+  height: 180,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Estefania",
+  Lastname: "Guerra",
+  email: "estefania123@gmail.com",
+  city: "berlin",
+  country: "alemania",
+  salary: 3000,
+  age: 40,
+  height: 150,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Aurora",
+  Lastname: "Espitia",
+  email: "estefania123@gmail.com",
+  city: "hamburgo",
+  country: "alemania",
+  salary: 3500,
+  age: 37,
+  height: 150,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Charrito",
+  Lastname: "Negro",
+  email: "charrito123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 2000,
+  age: 31,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Bahala",
+  Lastname: "Guantanamera",
+  email: "bahala123@gmail.com",
+  city: "jaipur",
+  country: "india",
+  age: 23,
+  height: 180,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Bahala",
+  Lastname: "Guantanamera",
+  email: "bahala123@gmail.com",
+  city: "jaipur",
+  country: "india",
+  age: 22,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Bahala",
+  Lastname: "Guantanamera",
+  email: "bahala123@gmail.com",
+  city: "jaipur",
+  country: "india",
+  age: 22,
+  height: 170,
+  weight: 120,
+});
+
+db.users.find();
+
+//1 Obtener todos los usuarios que sean mayores de 18 años.
+db.users.find({ age: { $eq: 18 } });
+
+//2 Obtener todos los usuarios que sean de Londres o de París.
+db.users.find({ city: { $in: ["londres", "paris"] } });
+
+//3 Obtener a todos los usuarios que ganen más de $2000 al mes y tengan menos de 30 años.
+db.users.find({ $and: [{ salary: { $gt: 2000 } }, { age: { $lt: 30 } }] });
+
+//4 Obtener a todos los usuarios que sean de España y ganen más de $3000 al mes.
+db.users.find({ $and: [{ country: { $in: ["españa"] } }, { salary: { $gt: 3000 } }] });
+
+//5 Obtener todos los usuarios que tengan entre 25 y 35 años.
+db.users.find({ $and: [{ age: { $gt: 25 } }, { age: { $lt: 35 } }] });
+
+//6 Obtener a todos los usuarios que no sean de Estados Unidos.
+db.users.find({ country: { $nin: ["estados unidos"] } });
+
+//7 Obtener a todos los usuarios que sean de Londres y que ganen más de $2500 o que tengan más de 30 años.
+db.users.find({ $and: [{ city: { $eq: "londres" } }, { $or: [{ salary: { $gt: 2500 } }, { age: { $gt: 30 } }] }] });
+
+//8 Obtener a todos los usuarios que sean de Australia y tengan un peso mayor a 140 libras.
+db.users.find({ $and: [{ country: { $eq: "australia" } }, { weight: { $gt: 140 } }] });
+
+//9 Obtener a todos los usuarios que no sean de Londres ni de París.
+db.users.find({ city: { $nin: ["londres", "paris"] } });
+
+//10 Obtener a todos los usuarios que ganen menos de $2000 al mes o que tengan más de 40 años.
+db.users.find({ $or: [{ salary: { $lt: 2000 } }, { age: { $gt: 40 } }] });
+
+//11 Obtener a todos los usuarios que sean de Canadá y ganen más de $4000 al mes o que tengan una altura mayor a 180 cm.
+db.users.find({
+  $and: [{ country: { $eq: "canada" } }, { $or: [{ salary: { $gt: 4000 } }, { weight: { $gt: 180 } }] }],
+});
+
+//12 Obtener todos los usuarios que sean de Italia y tengan entre 20 y 30 años.
+db.users.find({ $and: [{ country: { $eq: "italia" } }, { $and: [{ age: { $gt: 20 } }, { age: { $lt: 30 } }] }] });
+
+//13 Obtener todos los usuarios que no tengan un correo electrónico registrado.
+db.users.find({ email: { $exists: false } });
+
+//14 Obtener todos los usuarios que sean de Francia y que su salario esté entre $3000 y $5000 al mes.
+db.users.find({ country: { $eq: "francia" }, salary: { $gte: 3000, $lte: 5000 } });
+
+//15 Obtener todos los usuarios que sean de Brasil y que tengan un peso menor a 120 libras o más de 140 libras.
+db.users.find({ country: { $eq: "brasil" }, $or: [{ weight: { $lt: 120 } }, { weight: { $gt: 140 } }] });
+
+//16 Obtener todos los usuarios que sean de Argentina o de Chile y que tengan una edad menor a 25 años.
+db.users.find({ country: { $in: ["argentina", "chile"] }, age: { $lt: 25 } });
+
+//17 Obtener a todos los usuarios que no sean de España ni de México y que ganen menos de $3000 al mes.
+db.users.find({ country: { $nin: ["españa", "mexico"] }, salary: { $lt: 3000 } });
+
+//18 Obtener todos los usuarios que sean de Alemania y que tengan un salario menor a $4000 o una edad mayor a 35 años.
+db.users.find({ country: { $eq: "alemania" }, $or: [{ salary: { $lt: 4000 } }, { age: { $gt: 35 } }] });
+
+//19 Obtener todos los usuarios que no sean de Colombia y que su altura sea menor a 170 cm.
+db.users.find({ country: { $ne: "colombia" }, height: { $lt: 170 } });
+
+//20 Obtener todos los usuarios que sean de India y que no tengan un salario registrado.
+db.users.find({ country: { $eq: "india" }, salary: { $exists: false } });
+
+//21 Eliminar todos los usuarios que tengas 22 años
+db.users.deleteMany({ age: { $eq: 22 } });
+db.users.find({ age: { $eq: 22 } });
